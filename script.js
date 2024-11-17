461bba17708385e183f0ee43e1610b646f350454
Maxime Perreault <maxime.perreault@hotmail.fr>
2024-11-17 18:20:33 -0500

    work on the log html page + script
    not fetching idk what to do

--- script.js
+++ script.js
@@ -98,80 +98,118 @@ window.onload = (event) => {
             }
             // description
             document.getElementById("description").innerText = displayedProject.description;
-            // extra documentation
+            
+            // extra doc
             document.getElementById("doc-button-text").innerText = displayedProject.documentation[0].tag;
-            document.getElementById("doc-button").setAttribute('href', displayedProject.documentation[0].link);
+            document.getElementById("doc-button").addEventListener("click", function(event) {
+            const url = displayedProject.documentation[0].link;
+            const options = "width=800,height=600,scrollbars=yes,resizable=yes";
+            
+            if (displayedProject.documentation[0].tag == "Log") {
+                var newWindow = window.open("log.html", "_blank", options);
+                console.log(url)
 
-            // project buttons interaction
-            document.querySelectorAll('.project-button').forEach(button => {
-              button.addEventListener('click', function() {
-                // Remove Keywords and tools
-                document.querySelectorAll('.keyword').forEach(keyword => keyword.remove());
-                document.querySelectorAll('.tool').forEach(tool => tool.remove());
-                
-                // Remove and add 'active' class from all buttons
-                document.querySelectorAll('.project-button').forEach(btn => btn.classList.remove('active'));
-                this.classList.add('active');
-
-                // check which project is associated with the button
-                let currentTitle = this.childNodes[0].innerText;
-                for (let p = 0; p < projects.length; p++) {
-                  if (currentTitle === projects[p].title) {
-                    displayedProject = projects[p];
-                    // reset gallery buttons click counts
-                    currentImgIndex = 0;
-                    imgCount = displayedProject.gallery.length;
-
-                    // change the content of the page
-                    // title and year
-                    document.getElementById("title").innerText = projects[p].title;
-                    document.getElementById("year").innerText = projects[p].year;
-                    // display the first image
-                    galleryElement.src = projects[p].gallery[0].dir;
-                    // keywords
-                    for (let k = 0; k < projects[p].keywords.length; k++) {
-                      let currentKeyword = document.createElement("span");
-                      currentKeyword.classList.add("keyword");
-                      currentKeyword.innerText = projects[p].keywords[k];
-                      document.getElementById("keywords-box").appendChild(currentKeyword);
+                // Wait for the new window to load before modifying the text
+                newWindow.onload = function() {
+                  // Fetch the content of the text file
+                  fetch(url)
+                  .then(response => {
+                    if (!response.ok) {
+                      throw new Error('Failed to load the text file');
                     }
-                    // tools
-                    for (let t = 0; t < projects[p].tools.length; t++) {
-                      let currentTool = document.createElement("span");
-                      currentTool.classList.add("tool");
-                      currentTool.innerText = projects[p].tools[t];
-                      document.getElementById("tools-box").appendChild(currentTool);
-                    }
-                    // description
-                    document.getElementById("description").innerText = projects[p].description;
-                    // extra documentation
-                    document.getElementById("doc-button-text").innerText = projects[p].documentation[0].tag;
-                    document.getElementById("doc-button").setAttribute('href', projects[p].documentation[0].link);
-                  }
-                }
+                    return response.text();
+                  })
+                  .then(data => {
+                    // Find the .textlog element in the new window and set its text content
+                    
+                    console(data)
+                    
+                    var textbox = newWindow.document.querySelector(".textlog");
+                    if (textbox) {
+                          textbox.textContent = data;
+                      } else {
+                          console.error('Element .textlog not found in the new window.');
+                      }
+                    })
+                    .catch(error => {
+                      console.error('Error fetching the text file:', error);
+                    });
+                };
+            } else {
+                window.open(url, "_blank", options);
+            }
+          });
+
+          // project buttons interaction
+          document.querySelectorAll('.project-button').forEach(button => {
+            button.addEventListener('click', function() {
+              // Remove Keywords and tools
+              document.querySelectorAll('.keyword').forEach(keyword => keyword.remove());
+              document.querySelectorAll('.tool').forEach(tool => tool.remove());
               
-              });
-            });
+              // Remove and add 'active' class from all buttons
+              document.querySelectorAll('.project-button').forEach(btn => btn.classList.remove('active'));
+              this.classList.add('active');
 
-            // gallery buttons interaction
-            // right button
-            document.getElementById('right-gallery-button').addEventListener('click', function() {
-              // for each click, count up
-              currentImgIndex ++;
-              currentImgIndex = currentImgIndex % imgCount;
-              galleryElement.src = displayedProject.gallery[currentImgIndex].dir;
-            });
-            // left button
-            document.getElementById('left-gallery-button').addEventListener('click', function() {
-              // for each click, count down
-              currentImgIndex--;
-              if (currentImgIndex < 0) {
-                currentImgIndex = imgCount - 1;
+              // check which project is associated with the button
+              let currentTitle = this.childNodes[0].innerText;
+              for (let p = 0; p < projects.length; p++) {
+                if (currentTitle === projects[p].title) {
+                  displayedProject = projects[p];
+                  // reset gallery buttons click counts
+                  currentImgIndex = 0;
+                  imgCount = displayedProject.gallery.length;
+
+                  // change the content of the page
+                  // title and year
+                  document.getElementById("title").innerText = projects[p].title;
+                  document.getElementById("year").innerText = projects[p].year;
+                  // display the first image
+                  galleryElement.src = projects[p].gallery[0].dir;
+                  // keywords
+                  for (let k = 0; k < projects[p].keywords.length; k++) {
+                    let currentKeyword = document.createElement("span");
+                    currentKeyword.classList.add("keyword");
+                    currentKeyword.innerText = projects[p].keywords[k];
+                    document.getElementById("keywords-box").appendChild(currentKeyword);
+                  }
+                  // tools
+                  for (let t = 0; t < projects[p].tools.length; t++) {
+                    let currentTool = document.createElement("span");
+                    currentTool.classList.add("tool");
+                    currentTool.innerText = projects[p].tools[t];
+                    document.getElementById("tools-box").appendChild(currentTool);
+                  }
+                  // description
+                  document.getElementById("description").innerText = projects[p].description;
+                  // extra documentation
+                  document.getElementById("doc-button-text").innerText = projects[p].documentation[0].tag;
+                  document.getElementById("doc-button").setAttribute('href', projects[p].documentation[0].link);
+                }
               }
-              galleryElement.src = displayedProject.gallery[currentImgIndex].dir;
+            
             });
+          });
 
-            break;
+          // gallery buttons interaction
+          // right button
+          document.getElementById('right-gallery-button').addEventListener('click', function() {
+            // for each click, count up
+            currentImgIndex ++;
+            currentImgIndex = currentImgIndex % imgCount;
+            galleryElement.src = displayedProject.gallery[currentImgIndex].dir;
+          });
+          // left button
+          document.getElementById('left-gallery-button').addEventListener('click', function() {
+            // for each click, count down
+            currentImgIndex--;
+            if (currentImgIndex < 0) {
+              currentImgIndex = imgCount - 1;
+            }
+            galleryElement.src = displayedProject.gallery[currentImgIndex].dir;
+          });
+
+          break;
       }
 
     });
